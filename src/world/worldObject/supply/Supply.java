005f7cc32ae8ee601530f1f2ca4091beceadfeaa
HajagosNorbert <hajagosnorbi@gmail.com>
2021-04-23 18:07:36 +0200

    asdasdasd

--- src/world/worldObject/supply/Supply.java
+++ src/world/worldObject/supply/Supply.java
@@ -2,6 +2,22 @@ package world.worldObject.supply;
 
 import world.Illustratable;
 
-public interface Supply extends Illustratable {
+import java.util.Map;
+
+public class Supply extends Illustratable {
     //LEEGYSZERŰSÍTENI A SOK KÜLÖNBÖZ SUPPLY-T
+
+    Map<Resource, Integer> resourceAmounts;
+    static String illustration;
+    public Supply(Map<Resource, Integer> resourceAmounts, String illustration){
+        this.resourceAmounts = resourceAmounts;
+        this.illustration = getIllustration();
+    }
+
+    public Map<Resource, Integer> getResourceAmounts(){
+
+    }
+    public String getIllustration(){
+        return "a";
+    }
 }
